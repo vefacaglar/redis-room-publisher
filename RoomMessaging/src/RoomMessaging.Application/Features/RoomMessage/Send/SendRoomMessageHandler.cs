5ba0ab9127ce722e85820c8eddc05989dff97d96
Vefa Caglar Ozcan <vefacaglarozcan@gmail.com>
2026-02-16 11:53:04 +0300

    broadcast feature integrate with message handler

--- RoomMessaging/src/RoomMessaging.Application/Features/RoomMessage/Send/SendRoomMessageHandler.cs
+++ RoomMessaging/src/RoomMessaging.Application/Features/RoomMessage/Send/SendRoomMessageHandler.cs
@@ -1,5 +1,6 @@
 using MediatR;
 using Microsoft.Extensions.Logging;
+using RoomMessaging.Application.Messaging;
 
 namespace RoomMessaging.Application.Features.RoomMessage.Send
 {
@@ -13,17 +14,21 @@ namespace RoomMessaging.Application.Features.RoomMessage.Send
     public class SendRoomMessageNotificationHandler : INotificationHandler<SendRoomMessageNotification>
     {
         private readonly ILogger<SendRoomMessageNotificationHandler> _logger;
+        private readonly IBroadcastService _broadcastService;
 
         public SendRoomMessageNotificationHandler(
-            ILogger<SendRoomMessageNotificationHandler> logger
+            ILogger<SendRoomMessageNotificationHandler> logger,
+            IBroadcastService broadcastService
         )
         {
+            _broadcastService = broadcastService;
             _logger = logger;
         }
 
         public async Task Handle(SendRoomMessageNotification notification, CancellationToken cancellationToken)
         {
             _logger.LogInformation("Sending message to room {RoomName}: {Message}", notification.Name, notification.Message);
+            await _broadcastService.BroadcastAsync(notification.Name, notification.Message);
         }
     }
 }
